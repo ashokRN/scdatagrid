1f503029cd54cec63222bb06c5bea729be6f2ffb
Ashok <ashokarndev@gmail.com>
2024-07-23 13:15:33 +0530

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 const path = require('path');
-
+const MiniCssPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: './src/index.ts',
     mode : "production",
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
-                use: ['ts-loader'],
+                test: /\.(ts|tsx)$/,
+                use: {
+                    loader : "babel-loader",
+                    options:{
+                        presets:[
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript"
+                        ]
+                    }
+                },
                 exclude: /node_modules/,
             },
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [MiniCssPlugin.loader, "css-loader"],
             },
         ],
     },
@@ -20,10 +29,11 @@ module.exports = {
         "react": "react"
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.css'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [new MiniCssPlugin({filename : 'style.css'})]
 };
